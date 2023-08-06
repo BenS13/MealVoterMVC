d4a28ea687428c7e8652674ea03540330c7b2b05
Ben Sabatier (sabatierb) <bensab62@gmail.com>
2023-08-06 17:15:07 +0100

    New: Manage Avaliable Meals Controller and View created

---
+++ MealVoterMVC/Controllers/ManageMealsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MealVoterMVC.Controllers
+{
+    public class ManageMealsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
