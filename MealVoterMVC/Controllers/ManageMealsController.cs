89b4a3da20ce68f06fd43cbc044a1daf598c5717
Ben Sabatier (sabatierb) <bensab62@gmail.com>
2023-08-06 18:06:13 +0100

    Update: Adding a new meal now works, added some comments

--- MealVoterMVC/Controllers/ManageMealsController.cs
+++ MealVoterMVC/Controllers/ManageMealsController.cs
@@ -7,15 +7,30 @@ namespace MealVoterMVC.Controllers
     {
         public static List<Meal> Meals = new List<Meal>();
 
-        //Get: "/ManageMeals"
+        //GET: "/ManageMeals"
+        //Passes List<Meal> Meals list into View
         public IActionResult Index()
         {
             return View(Meals);//Returns View /ManageMeals/Index.cshtml
         }
 
+        //GET: "/ManageMeals/Create
+        //Renders form to add a new meal
+        [HttpGet]
         public IActionResult Create()
         {
             return View();//Returns View /ManageMeals/Create.cshtml
+        }
+
+        //POST: "/ManageMeals/CreateNew"
+        //Takes new Meal object as input parameter from View
+        //Adds Meal object to list of Meal objects
+        //Will connect DB soon
+        [HttpPost]
+        public IActionResult CreateNew(Meal _newMeal)
+        {
+            Meals.Add(_newMeal);//Add new Meal object to List(Meals)
+            return RedirectToAction("Index");//Redirect to index page where List(Meals) displayed
         }
     }
 }
