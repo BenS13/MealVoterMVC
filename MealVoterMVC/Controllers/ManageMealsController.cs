11a5ffdb0d2e4a6f705f2676c66b7c7df37f54bb
Ben Sabatier (sabatierb) <bensab62@gmail.com>
2023-08-06 17:49:30 +0100

    New: Create new meal View added

--- MealVoterMVC/Controllers/ManageMealsController.cs
+++ MealVoterMVC/Controllers/ManageMealsController.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MealVoterMVC.Models;
 
 namespace MealVoterMVC.Controllers
 {
     public class ManageMealsController : Controller
     {
+        public static List<Meal> Meals = new List<Meal>();
+
         //Get: "/ManageMeals"
         public IActionResult Index()
         {
-            return View();//Returns View /ManageMeals/Index.cshtml
+            return View(Meals);//Returns View /ManageMeals/Index.cshtml
+        }
+
+        public IActionResult Create()
+        {
+            return View();//Returns View /ManageMeals/Create.cshtml
         }
     }
 }
